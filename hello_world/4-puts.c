0c5e7b2a5efb7e7db171efee44918257505f07b6
root <Francoherrera123>
2023-02-15 09:11:44 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle"); | return(0);
+#include<stdio.h>
+
+int main(){
+	char Str[] = "\"Programming is like building a multilingual puzzle";
+	puts(Str);  
+	return(0);
+}
