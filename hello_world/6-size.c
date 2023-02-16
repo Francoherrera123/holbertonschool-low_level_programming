91f11639e2390c446f4b24b144d3ad92604d646d
root <Francoherrera123>
2023-02-16 05:07:49 -0800

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	printf("\n");
 
 	return (0);
 
