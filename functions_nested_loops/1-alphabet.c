c7d8a049c57d4093c385ce76ee6a8d651725d152
root <Francoherrera123>
2023-02-17 09:19:31 -0800

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-s
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+
+}
