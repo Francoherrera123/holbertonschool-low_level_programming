7dd17249af9c923f703d411bde9db504435b94aa
root <Francoherrera123>
2023-02-17 06:49:59 -0800

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,15 +12,12 @@ main(void) /*this main does something */
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i != 9)
+		putchar(i);
+
+		if (i < 9)
 		{
-			putchar(i);
 			putchar(44);
 			putchar(32);
-		}
-		else
-		{
-			putchar(i);
 		}
 	}
 		putchar('\n');
