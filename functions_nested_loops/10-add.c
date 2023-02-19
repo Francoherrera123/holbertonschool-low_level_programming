b8ecf04dc4d5d964d0f5fb160fc6d38f9761bc58
root <Francoherrera123>
2023-02-18 19:50:50 -0800

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * add - Entry point
+ * @val1: integer 1
+ * @val2: integer 2
+ * Return: Always 0 (Success)
+ */
+int add(int val1, int val2)
+{
+	int SUM = val1 + val2 
+	return (SUM);
+}
