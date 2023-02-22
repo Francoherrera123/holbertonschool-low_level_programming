346aafdbe8db214a64ab2444e94ad45ec8232060
root <Francoherrera123>
2023-02-22 07:18:51 -0800

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Returns 1 if c is a digit
+ *
+ *@a: number
+ *@b: number
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
