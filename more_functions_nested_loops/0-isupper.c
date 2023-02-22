0bcd0c6dc61230d4e04086fc4a148bb851cb3e04
root <Francoherrera123>
2023-02-22 07:00:19 -0800

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Returns 1 if c is uppercase
+ *
+ *@c: character
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1)
+	}
+
+    return (0);
+}
