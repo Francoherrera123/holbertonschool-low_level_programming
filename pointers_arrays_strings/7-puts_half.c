9584fbccee2dcb4ef29cac84ee57b0d995b33d38
root <Francoherrera123>
2023-02-28 06:36:08 -0800

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  puts_half - prints a string and reverse it
+ *  @str: char
+ */
+void puts_half(char *str)
+{
+	int lenght = 0;
+
+	int i = 0;
+
+	while (str[lenght])
+	{
+		lenght++;
+	}
+
+	i = lenght;
+
+	i = lenght / 2;
+
+	while (str[i])
+	{
+		_putchar(str[lenght]);
+		i++;
+	}
+}
