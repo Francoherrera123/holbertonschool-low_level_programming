3c37d555ae3f8ee0ad3984f6585ac9fc95d80c4e
root <Francoherrera123>
2023-02-28 09:22:23 -0800

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -7,7 +7,7 @@
  */
 void print_array(int *a, int n)
 {
-	int b, c;
+	int b, int c;
 
 	for (b = 0; b < n; b++)
 	{
@@ -17,7 +17,7 @@ void print_array(int *a, int n)
 			printf(",");
 			printf(" ");
 		}
-		
+
 	}
 	 _putchar('\n');
 }
