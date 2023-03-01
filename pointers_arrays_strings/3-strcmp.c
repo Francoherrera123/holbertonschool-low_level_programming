990beab23f8f90b5a71f4fbacaa88f81436e4581
root <Francoherrera123>
2023-03-01 10:05:09 -0800

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
- * _strcmp - Copies the string pointed to by src
+ * _strcmp - Compares strings
  *
- * @s1: *char
- * @s2: *char
- * Return: Pointer to destiny
+ * @s1: First string
+ * @s2: Second string
+ * Return: 0
  */
 
 int _strcmp(char *s1, char *s2)
-{	
+{
 	while (*s1 && *s2)
-	{	
+	{
 		if (*s1 != *s2)
 			return (*s1 - *s2);
 
 		s1++;
 		s2++;
+
 	}
+
 	return (0);
-	
+
 }
