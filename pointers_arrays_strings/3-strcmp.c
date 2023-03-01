34213d998bae9a6a8456c03943e26f6e7829703b
root <Francoherrera123>
2023-03-01 09:52:12 -0800

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - Copies the string pointed to by src
+ *
+ * @s1: *char
+ * @s2: *char
+ * Return: Pointer to destiny
+ */
+
+int _strcmp(char *s1, char *s2)
+{	
+	
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+	
+	return (0);
+	
+}
