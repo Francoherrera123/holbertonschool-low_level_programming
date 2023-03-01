677939150e881154a91dfdca9907d6d73361ccf2
root <Francoherrera123>
2023-03-01 08:03:06 -0800

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strncat - Copies the string pointed to by src
+ *
+ * @dest: *char
+ * @src: *char
+ * @n: int
+ * Return: Pointer to destiny
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destCont = 0;
+	int contSrc = 0;
+
+	while (dest[destCont])
+		destCont++;
+
+	while (src[contSrc] < n)
+	{
+		dest[destCont] = src[contSrc];
+		destCont++;
+
+		if (src[contSrc] == '\0')
+			break;
+
+		contSrc++;
+	}
+
+	return (dest);
+}
