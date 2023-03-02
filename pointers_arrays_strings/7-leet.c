f25f09af41a54f7735420b4931f515b6472d619f
root <Francoherrera123>
2023-03-02 08:57:28 -0800

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ * *leet: chage some letters
+ * @rucula: *char
+ * return: encode 1337
+ */
+char *leet(char *rucula)
+{
+	int count;
+	int count2;
+
+	char change[] = {'4','4','3','3','0','0','7','7','1','1'};
+	char check[] = {'a','A','e','E','o','O','t','T','l','L'};
+
+	for (count = 0; rucula[count]; count++)
+	{
+		for (count2 = 0; check[count2]; count2++)
+			if (rucula[count] == check[count2])
+				rucula[count] = change[count2];
+	}
+	return (rucula);
+}
