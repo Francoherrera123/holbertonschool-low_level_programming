1817a66ceba76842a27e7fdd0209f1f8aaed23d2
root <Francoherrera123>
2023-03-02 05:52:17 -0800

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * *cap_string - Always look up
+ *
+ * @upp: string to uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *upp)
+{
+	int recor = 0;
+
+	while (upp[recor])
+	{
+		if (upp[cont] >= 97 && upp[cont] <= 122 == upp[0])
+			upp[recor] -= 32;
+
+		if (upp[cont] >= 97 && upp[cont] <= 122)
+		{
+			switch (upp[recor] -1)
+			{
+				case 32:
+				case 44:
+				case 59:
+				case 46:
+				case 10:
+				case 9:
+				case 33:
+				case 63:
+				case 34:
+				case 40:
+				case 41:
+				case 123:
+				case 125:
+					upp[recor] -= 32;
+			}
+		}
+		
+		recor++;
+	}
+	return (upp);
+}
