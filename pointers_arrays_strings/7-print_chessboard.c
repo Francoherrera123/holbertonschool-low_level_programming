bbab7dccdee8d4a0ea30ae0a66e8f1ba93974808
root <Francoherrera123>
2023-03-03 01:40:39 -0800

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -13,12 +13,12 @@ void print_chessboard(char (*a)[8])
 
 	for (column = 0; column < 8; column++)
 	{
-			for (row = 0; row < 8; row++)
-			{
-				_putchar(a[column][row]);
-			}
+		for (row = 0; row < 8; row++)
+		{
+			_putchar(a[column][row]);
+		}
 
-		 	_putchar(10);
+		_putchar(10);
 	}
 }
 
