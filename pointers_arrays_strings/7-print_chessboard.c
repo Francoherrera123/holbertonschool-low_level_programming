66b6e209689ba0c527ec50c812238af6a79c1143
root <Francoherrera123>
2023-03-03 01:37:47 -0800

    7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_chessboard - Prints the pieces of the chess.
+ *
+ * @a: *char
+ * Return: void
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int column;
+	int row;
+
+	for (column = 0; column < 8; column++)
+	{
+			for (row = 0; row < 8; row++)
+			{
+				_putchar(a[column][row]);
+			}
+
+		 	_putchar(10);
+	}
+}
+
