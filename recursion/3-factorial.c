2b74743d66cd3519358237ab898150c669cdc826
root <Francoherrera123>
2023-03-06 04:25:42 -0800

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * factorial - factorial of a number
+ * @n: integer pointer
+ * Return: returns the factorial of a given number.
+ */
+int factorial(int n)
+{
+	return (n < 0 ? (factorial(n - 1) * n) : n == 0 ? 1 : -1);
+}
