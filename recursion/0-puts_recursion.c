a464bcd7c839df26168ea4a611555864f14c77ba
root <Francoherrera123>
2023-03-05 19:39:46 -0800

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: char pointer
  */
 
-void _puts_recursion(char*s)
+void _puts_recursion(char *s)
 {
 	if (s[0] == '\0')
 	{
