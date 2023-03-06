6436dc7bd6b57726d85453abfc672ba2196a3a93
root <Francoherrera123>
2023-03-05 19:43:23 -0800

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,15 +7,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (s[0])
 	{
-		_putchar('\n');
+		_putchar(s[0]);
+		_puts_recursion(&s[1]);
 	}
 
-	if (*s != '\n')
+	else
 	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
+		_putchar('\n');
 	}
 }
