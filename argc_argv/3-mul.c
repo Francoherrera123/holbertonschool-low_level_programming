773e07d3c0c4352ec003324391f69b9ff6c2b780
root <Francoherrera123>
2023-03-07 09:40:16 -0800

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,10 +8,14 @@
 int main(int argc, char *argv[])
 {
 	int sum;
+	int value1;
+	int value2;
 
 	if (argv[1] != NULL && argv[2] != NULL)
 	{
-		sum = argv[1] * argv[2];
+		value1 = argv[1];
+		value2 = argv[2];
+		sum = value1 * value2;
 		printf("%d\n", sum);
 		return (0);
 	}
