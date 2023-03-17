84c8304c04ee7bfb0270355d968066a08f9d8e17
root <Francoherrera123>
2023-03-17 06:52:34 -0700

    4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *free_grid - Entry Point
+ *@grid: int
+ *@height: int
+ */
+void free_grid(int **grid, int height)
+{
+	int cont;
+
+	for (cont = 0, cont < height; cont++)
+		free(grid[cont]);
+
+	free(grid);
+}
