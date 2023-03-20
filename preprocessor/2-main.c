dba351de51c2ecd291d5969a133d3f032d558b9a
root <Francoherrera123>
2023-03-19 19:07:55 -0700

    preprocessor

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define FILE_NAME __FILE__
+
+/**
+ * main - Entry Point
+ * Return: Return 0 Success
+ */
+
+int main()
+{
+	printf("%s\n", FILE_NAME);
+	return (0);
+}
---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef Task2
+#define Task2
+
+#define ABS(x) ((x) < 0 ? (x) * -1 : (x))
+#endif
