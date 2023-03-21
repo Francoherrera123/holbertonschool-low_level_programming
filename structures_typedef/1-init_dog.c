57f5775a15901dda944dace00f2b1595feb62afd
root <Francoherrera123>
2023-03-21 08:53:54 -0700

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - params constructor
+ * @d: pointer
+ * @name: 1st element
+ * @age: 2nd element
+ * @owner: 3rd element
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		exit(0);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
