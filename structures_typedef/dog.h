f725d2fa831b8cb99657f083f13335cabfb540fe
root <Francoherrera123>
2023-03-21 14:03:51 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,6 +19,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 /*task 2*/
 void print_dog(struct dog *d);
 
+/* task 3*/
+typedef struct dog dog_t:
 
+/* task 4*/
+dog_t *new_dog(char *name, float age, char *owner);
+/* task 5*/
+void free_dog(dog_t *d);
 #endif
 
