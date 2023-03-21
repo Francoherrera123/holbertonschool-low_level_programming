b9e464b7dfa2bcb097aec83e81097b26b02918ac
root <Francoherrera123>
2023-03-21 08:41:02 -0700

    structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * struct dog - params
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd  member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
+
