d2c5ef5ea9cf0c9f57e1841846e32aafc52048f4
root <Francoherrera123>
2023-03-21 20:25:32 -0700

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - create dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ *
+ * Return: dog location
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int save_owner, save_name, i;
+
+	dog_t *point;
+
+	point = malloc(sizeof(dog_t));
+
+	if (point->name == NULL)
+		return (NULL);
+
+	save_owner = strlen(owner);
+	save_name = strlen(name);
+	point->name = malloc(save_name + 1);
+	{
+		if (point->name == NULL)
+		{
+			free(point);
+			return (NULL);
+		}
+	}
+	point->owner = malloc(save_owner + 1);
+	{
+		if (point->owner == NULL)
+		{
+			free(point->name);
+			free(point);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < save_name; i++)
+		point->name[i] = name[i];
+	point->name[i] = '\0';
+
+	for (i = 0; i < save_owner; i++)
+		point->owner[i] = owner[i];
+
+	point->age = age;
+	return (point);
+}
