3d7ffbe712acddf1be9dc22be309cc16d6586c29
root <Francoherrera123>
2023-03-23 15:16:56 -0700

    3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -9,7 +9,7 @@
  * Return: int
  */
 int (*get_op_func(char *s))(int, int)
-{	
+{
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -19,10 +19,10 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-    int i = 0;
+	int i = 0;
 
 	while (ops[i].op != NULL)
-	{	
+	{
 		if (*s == ops[i].op[0])
 			return (ops[i].f);
 		i++;
