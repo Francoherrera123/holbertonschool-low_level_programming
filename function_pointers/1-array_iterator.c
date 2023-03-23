aa75b7fe99a1e5796621ccba3a03ae8d3cb75340
root <Francoherrera123>
2023-03-23 09:00:02 -0700

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Entry Point
+ *
+ * @array: Data structure.
+ * @size: size of the array
+ * @action: function passed by parameters
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	unsigned int cont;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (cont = 0; cont < size; cont++)
+		(*action)(array[cont])
+}
