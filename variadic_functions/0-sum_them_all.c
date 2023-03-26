082ea5742554ff22a9ee4ab1c53378c66e42e5e9
root <Francoherrera123>
2023-03-26 16:55:22 -0700

    structure

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg>
+
+/**
+ * sum_them_all - Entry point
+ *
+ * @n: const unsigned int
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int cont = 0;
+	int sum = 0;
+
+	va_start(list, n);
+
+	for (; cont < n; cont++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+	return (sum);
+}
