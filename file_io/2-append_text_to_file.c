f67fa672b575df729031a23743552973d3392e23
root <Francoherrera123>
2023-04-12 18:46:20 -0700

    Task 3

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * append_text_to_file - creates a file
+ * @filename: file name
+ * @text_content: is a string
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_APPEND | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+	return (1);
+}
