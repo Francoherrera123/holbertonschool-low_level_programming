07ce97b6319f34e58e24611c0f3145f5b2651a3e
root <Francoherrera123>
2023-04-12 18:45:59 -0700

    Task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file
+ * @filename: file name
+ * @text_content: is a string
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!*filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+	return (1);
+
+}
