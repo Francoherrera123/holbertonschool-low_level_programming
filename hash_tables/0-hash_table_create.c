8e0171c77a014eae5e99393b2ea0a215cd810f97
root <Francoherrera123>
2023-04-16 09:53:57 -0700

    Task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = malloc (sizeof(hash_table_t));
+	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 	ht->size = size;
