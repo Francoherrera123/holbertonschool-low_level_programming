c253bfd4e004c420eda36ab4884d40c45acb05f7
root <Francoherrera123>
2023-04-16 09:06:02 -0700

    Task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: Is the hash table you want to add
+ * or update the key/value to
+ * @key: is the key, cannot be empty
+ * @value: is the value assosiated to the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = NULL;
+	char *valuedup = strdup(value);
+	char *keydup = strdup(key);
+	unsigned int index = 0;
+
+	if (!ht)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+
+	node->key = keydup;
+	node->value = valuedup;
+
+	if (ht->array[index] == NULL || (strcmp(ht->array[index]->key, key) == 0))
+	{
+		if (ht->array[index])
+		{
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
+			free(ht->array[index]);
+		}
+		ht->array[index] = node;
+	}
+	else
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+	return (1);
+}
